73014cdc53912cd171109bd7332cf58f951f210c
Bartosz Bogacz <bartosz.bogacz@mailbox.org>
2021-01-06 13:23:35 +0100

    Begin fixing stacking order.

--- src/stackable.ts
+++ src/stackable.ts
@@ -7,6 +7,13 @@ function stackablesSynchronize(local: LocalGame, remote: RemoteGame) {
 }
 
 function stackablesCompute(local: LocalGame) {
+  // TODO: Currently local.locatables is used for both remote synchronization
+  // and local visualisation. On every frame locatables are transformed by
+  // stackings and stratifiers for proper layouting, and the only reason
+  // those changes are not propagated to all remote peers is that tick
+  // and ownedBy are not updated. Such a scheme will fail the moment
+  // we switch to an automatic detection of changes.
+
   local.stacks = {};
 
   for (const [key, st] of Object.entries(local.stackables)) {
@@ -53,6 +60,8 @@ function stackablesKeyUp(local: LocalGame, itemId: string) {
 
 function stackablesTake(local: LocalGame, itemId: string) {
   if (local.stackables.hasOwnProperty(itemId)) {
+    local.stackables[itemId].tick = local.tick + 1;
+    local.stackables[itemId].ownedBy = local.playerId;
     local.stackables[itemId].onStacking = null;
   }
 }
@@ -78,10 +87,39 @@ function stackablesPlace(
     if (largest !== null) {
       let stackingId = local.stackables[largest].onStacking;
       if (stackingId === null) {
+        // FIXME: Freshly created stack, local.stacks does not
+        // yet contain it, however, stackablesFindGap requires
+        // it to find a suitable x value. Current workaround
+        // uses an initial x.
         stackingId = stackingsCreateFor(local, largest);
+        local.stackables[largest].tick = local.tick + 1;
+        local.stackables[largest].ownedBy = local.playerId;
         local.stackables[largest].onStacking = stackingId;
+
+        local.locatables[largest].tick = local.tick + 1;
+        local.locatables[largest].ownedBy = local.playerId;
+        local.locatables[largest].x = 0;
+
+        local.stackables[itemId].tick = local.tick + 1;
+        local.stackables[itemId].ownedBy = local.playerId;
+        local.stackables[itemId].onStacking = stackingId;
+
+        local.locatables[itemId].tick = local.tick + 1;
+        local.locatables[itemId].ownedBy = local.playerId;
+        local.locatables[itemId].x = 1;
+      } else {
+        local.stackables[itemId].tick = local.tick + 1;
+        local.stackables[itemId].ownedBy = local.playerId;
+        local.stackables[itemId].onStacking = stackingId;
+
+        local.locatables[itemId].tick = local.tick + 1;
+        local.locatables[itemId].ownedBy = local.playerId;
+        local.locatables[itemId].x = stackablesFindGap(
+          local,
+          stackingId,
+          itemId
+        );
       }
-      local.stackables[itemId].onStacking = stackingId;
     }
   }
 }
@@ -105,4 +143,27 @@ function stackablesFindOverlapping(
   }
 
   return largest;
+}
+
+function stackablesFindGap(
+  local: LocalGame,
+  stackingId: string,
+  itemId: string
+): number {
+  if (local.stacks === null) {
+    return 0;
+  }
+
+  let posX: number = 0;
+
+  for (const otherId of local.stacks[stackingId]) {
+    if (local.locatables[otherId].x > local.locatables[itemId].x) {
+      return (posX + local.locatables[otherId].x) / 2;
+    } else {
+      posX = local.locatables[otherId].x;
+    }
+  }
+
+  // Reached last stackable. Put the new stackable behind that one.
+  return posX + 1;
 }
